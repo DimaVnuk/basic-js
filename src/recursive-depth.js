74bdf65a1b477b5a51639dbb5141f543d742df43
DimaVnuk <71352399+DimaVnuk@users.noreply.github.com>
2021-09-26 21:31:18 +0300

    Update recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -14,5 +14,5 @@ import { NotImplementedError } from "../extensions/index.js";
  */
 export default class DepthCalculator {
   calculateDepth( arr ) {
-   return Array.isArray(arr) ?  1 + arr.reduce((a, b) => Math.max(a, this.calculateDepth(b)), 0) : 0
+   return Array.isArray(arr) ? 1 + arr.reduce((a, b) => Math.max(a, this.calculateDepth(b)), 0) : 0
 }
