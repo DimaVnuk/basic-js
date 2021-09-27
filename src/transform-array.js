1d5dd835cefb58e0a1274bb458e311df6b9982ab
DimaVnuk <vnuchok311994@hotmail.com>
2021-09-27 19:57:26 +0300

    feat: add transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,63 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let newArr = [...arr];
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (
+      newArr.indexOf("--double-prev") - newArr.indexOf("--discard-next") ===
+        2 ||
+      newArr.indexOf("--discard-prev") - newArr.indexOf("--discard-next") === 2
+    ) {
+      const indexNext = newArr.indexOf("--discard-next") + 1;
+      const index = newArr.indexOf("--discard-next");
+      const prev = newArr.indexOf("--discard-next") + 2;
+      if (index > -1 && indexNext > -1 && prev > -1) {
+        newArr.splice(prev, 1);
+        newArr.splice(indexNext, 1);
+        newArr.splice(index, 1);
+        return newArr;
+      }
+    }
+    if (
+      newArr.indexOf("--double-next") === newArr.length - 1 ||
+      newArr.indexOf("--discard-next") === newArr.length - 1
+    ) {
+      newArr.splice(newArr.length - 1, 1);
+
+      return newArr;
+    }
+    if (newArr[i] === "--double-next") newArr[i] = newArr[i + 1];
+    if (newArr[i] === "--discard-next") {
+      const indexNext = newArr.indexOf(newArr[i + 1]);
+      const index = newArr.indexOf(newArr[i]);
+      if (index > -1 && indexNext > -1) {
+        newArr.splice(indexNext, 1);
+        newArr.splice(index, 1);
+        return newArr;
+      }
+    }
+    if ("--double-prev" === newArr[0] || "--discard-prev" === newArr[0]) {
+      newArr.splice(newArr[0], 1);
+      return newArr;
+    }
+    if (newArr[i] === "--double-prev") {
+      newArr[i] = newArr[i - 1];
+    }
+
+    if (newArr[i] === "--discard-prev") {
+      const index = newArr.indexOf(newArr[i]);
+      const indexPrev = newArr.indexOf(newArr[i - 1]);
+      if (index > -1 && indexPrev > -1) {
+        newArr.splice(index, 1);
+        newArr.splice(indexPrev, 1);
+        return newArr;
+      }
+    }
+  }
+  return newArr;
 }
