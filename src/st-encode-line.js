c32cccfb11ac287f933a4144e8979308a3237611
DimaVnuk <vnuchok311994@hotmail.com>
2021-09-30 23:55:26 +0300

    feat: add encode line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,4 +1,4 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Given a string, return its encoding version.
@@ -10,6 +10,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-    throw new NotImplementedError('Not implemented');
+export default function encodeLine(str) {
+  let newString = ``;
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) count++;
+    else if (count === 1) newString += `${str[i]}`;
+    else {
+      newString += `${count}${str[i]}`;
+      count = 1;
+    }
+  }
+  return newString;
 }
