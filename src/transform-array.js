f4bf512828fa18c9cd7cb9b8ee6d80249c167911
DimaVnuk <vnuchok311994@hotmail.com>
2021-10-02 17:17:20 +0300

    feat: add extended rep

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,24 +1,24 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
 export default function transform(arr) {
   if (!Array.isArray(arr)) {
     throw new Error("'arr' parameter must be an instance of the Array!");
   }
   let newArr = [...arr];
-
+  
   for (let i = 0; i < newArr.length; i++) {
     if (
       newArr.indexOf("--double-prev") - newArr.indexOf("--discard-next") ===
